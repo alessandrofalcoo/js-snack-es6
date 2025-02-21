ffd9255316f48aa19d054942e44933814c4521e3
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-21 17:23:27 +0100

    add comments

--- main.js
+++ main.js
@@ -44,6 +44,8 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
 
+
+// Creo un array di oggetti con dentro tre proprietà ciascuno 
 const teams = [
     {
         name: 'Inter',
@@ -62,12 +64,17 @@ const teams = [
     },
 ]
 
+
+// Itero sull'array di oggetti in modo da ottenere un numero random nelle proprietà dei punti e dei falli
 for (let i = 0; i < teams.length; i++) {
     teams[i].points = Math.floor(Math.random() * 10) + 1;
     teams[i].fouls = Math.floor(Math.random() * 10) + 1;
 }
+// Stampo l'array in console
 console.log(teams);
 
+
+// Creo un array di oggetti con dentro solo due proprietà
 const namesFouls = [
     {
         name:'Inter',
@@ -82,9 +89,13 @@ const namesFouls = [
         fouls: 0        
     },
 ]
+
+// Itero sull'array in modo da generare un numero random nella fouls property
 for (let i = 0; i < namesFouls.length; i++) {
     namesFouls[i].fouls = Math.floor(Math.random() * 10) + 1;
 }
+
+// Stampo l'array in console
 console.log(namesFouls);
 
 
